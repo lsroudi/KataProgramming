b02dc79cc607abe0ad50a7aa92e4a768f6f855c4
El akroudi abdessamad <lsroudi@gmail.com>
2015-05-17 21:49:26 +0200

    add LinkedQueueOfString implementation

---
+++ LinkedQueueOfString.java
@@ -0,0 +1,44 @@
+package KataProgramming;
+
+
+/**
+ * Description of LinkedQueueOfString
+ *
+ * (c) lsroudi  <lsroudi@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+public class LinkedQueueOfString {
+
+	private Node first, last;
+	
+	public class Node
+	{
+		String item;
+		Node next;
+	}
+	
+	public boolean isEmpty(){
+		return first == null;
+	}
+	
+	public void enqueue(String item)
+	{
+		Node oldLast = last;
+		last = new Node();
+		last.item = item;
+		last.next = null;
+		
+		if(isEmpty()) first = last;
+		else 	oldLast.next = last;
+	}
+	
+	public String dequeue(){
+		String item = first.item;
+		first = first.next;
+		if(isEmpty()) last = null;
+		
+		return item;
+	}
+}
